c083b8b6df5815e2f582fcc0bf604797e76c2e7e
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-04-08 09:31:49 +0100

    ✅ Implement the rss-to-json endpoint logic

--- api/v1/controllers/rssToJSONController.js
+++ api/v1/controllers/rssToJSONController.js
@@ -1,5 +1,18 @@
+const axios = require('axios');
+
 const catchAsync = require('../../../utils/catchAsync');
+const AppError = require('../../../utils/appError');
 
 exports.convertRSStoJSON = catchAsync(async (req, res, next) => {
-  console.log('RESPONSE');
+  const { query } = req;
+
+  if (!query.rss_url) {
+    return next(new AppError('`rss_url` parameter is required.', 400));
+  }
+
+  const rss2jsonUrl = `${process.env.RSS2JSON_BASE_URL}?rss_url=${query.rss_url}`;
+
+  const result = await axios.get(rss2jsonUrl);
+
+  res.status(200).json(result.data);
 });
