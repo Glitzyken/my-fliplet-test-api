1dd1a95cc12304dfb453f54b3b8bd7d9b2764622
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-04-08 08:22:46 +0100

    🔐 Initialize the Express app - Configured Dotenv - Handled uncaughtException and unhandledRejection - Handled SIGTERM to terminate program if requested - Activate security packages such as helmet for security best practices - Activate CORS

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const compression = require('compression');
+
+const app = express();
+
+app.enable('trust proxy');
+
+// GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// if (process.env.NODE_ENV === 'development') {
+app.use(morgan('dev'));
+// }
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit each IP to 100 requests per windowMs
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  message: 'Too many requests from this IP, please try again in 15 minutes!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+  console.log(`Node enviroment is ${process.env.NODE_ENV}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
+
+module.exports = server;
